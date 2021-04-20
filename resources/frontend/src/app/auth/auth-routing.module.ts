837ae6732b2177b33723e0ed3f3b8439594d4aca
Mario Alberto Cabrera Alfaro <maca.15c@gmail.com>
2021-04-20 15:31:59 -0500

    se empieza proceso de recuperacion de contrasenia

--- resources/frontend/src/app/auth/auth-routing.module.ts
+++ resources/frontend/src/app/auth/auth-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { GuessGuard } from './guess.guard';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent, canActivate: [GuessGuard] },
+  { path: 'forgot-password', component: ForgotPasswordComponent, canActivate: [GuessGuard] }
 ];
 
 @NgModule({
--- resources/frontend/src/app/auth/auth.module.ts
+++ resources/frontend/src/app/auth/auth.module.ts
@@ -5,10 +5,11 @@ import { SharedModule } from '../shared/shared.module';
 import { AuthRoutingModule } from './auth-routing.module';
 
 import { LoginComponent } from './login/login.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, ForgotPasswordComponent],
   imports: [
     ReactiveFormsModule,
     HttpClientModule,
